cba2878fd0bd3ec1dc80e26b6cbba4792c7842b1
olchik2006 <olavasulushun@gmail.com>
2026-05-04 20:14:31 +0300

    guard posthog init for ci

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,16 @@ import "./styles/main.css";
 import { renderApp } from "./App.js";
 import posthog from "posthog-js";
 
-posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
-  api_host: import.meta.env.VITE_POSTHOG_HOST,
-  person_profiles: "identified_only",
-  capture_pageview: true,
-  autocapture: true,
-});
+const key = import.meta.env.VITE_POSTHOG_KEY;
+const host = import.meta.env.VITE_POSTHOG_HOST;
+
+if (key && host) {
+  posthog.init(key, {
+    api_host: host,
+    person_profiles: "identified_only",
+    capture_pageview: true,
+    autocapture: true,
+  });
+}
 
 renderApp();
